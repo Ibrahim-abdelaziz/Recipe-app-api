30f06c3d49ad3821aa3df002098a2a5f899097c3
Ibrahim Abdelaziz <ibrahim.abdelaziz1000@gmail.com>
2020-05-22 18:32:39 +0200

    add Recipe Features

--- app/core/models.py
+++ app/core/models.py
@@ -56,3 +56,17 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+
+    def __str__(self):
+        return self.title
+
